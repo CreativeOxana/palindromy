11460dfca8383d810663d0792a26068529ee1d4e
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-27 18:05:35 +0200

    Added forEach

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-console.log('Funguju!')
-
-const textPozpatku = (text) => {
-	return text.split('').reverse().join('')
-}
-
 // textPozpatku('Ahoj světe') // 'etěvs johA'
 // textPozpatku('Kajak') // 'kajaK'
+
+// const textPozpatku = (text) => {
+//   return text.split('').reverse().join('');
+// };
+
+const textList = document.querySelectorAll('li');
+
+textList.forEach((item) => {
+  const text = item.textContent.toLowerCase().replace(/ /g, '');
+  if (text === text.split('').reverse().join('')) {
+    item.classList.add('palindrom');
+  }
+});
